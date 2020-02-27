2b9590af63bee95be3faae2dbf6c1c9595ed0247
rishikesh vedpathak <rishikesh.vedpathak@turtlemint.com>
2020-02-27 19:13:54 +0530

    Updates

---
+++ precache-manifest.bf6cbab7028d33e944e3e33903535019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eec20a10e5bbb366600bfbe71b8777",
+    "url": "/react-responsive-demo/index.html"
+  },
+  {
+    "revision": "d54eea300b56034f8dc2",
+    "url": "/react-responsive-demo/static/css/main.8a603f98.chunk.css"
+  },
+  {
+    "revision": "ec5ddbb018ed587d19b4",
+    "url": "/react-responsive-demo/static/js/2.58248d18.chunk.js"
+  },
+  {
+    "revision": "bdc7e82db52bf266d5536c19690b95ee",
+    "url": "/react-responsive-demo/static/js/2.58248d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54eea300b56034f8dc2",
+    "url": "/react-responsive-demo/static/js/main.e16d1656.chunk.js"
+  },
+  {
+    "revision": "74d0458f9185dcfec002",
+    "url": "/react-responsive-demo/static/js/runtime-main.6801ffa0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-responsive-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
